54fcd32289091f8d342f1909c6d0ab01072f72f5
EmelyanovSI <48243046+EmelyanovSI@users.noreply.github.com>
2019-03-09 16:21:36 +0300

    lab2 Initial commit

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -3,14 +3,14 @@
 	partial class Form1
 	{
 		/// <summary>
-		/// Обязательная переменная конструктора.
+		/// Required designer variable.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 
 		/// <summary>
-		/// Освободить все используемые ресурсы.
+		/// Clean up any resources being used.
 		/// </summary>
-		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing && (components != null))
@@ -20,162 +20,57 @@
 			base.Dispose(disposing);
 		}
 
-		#region Код, автоматически созданный конструктором форм Windows
+		#region Windows Form Designer generated code
 
 		/// <summary>
-		/// Требуемый метод для поддержки конструктора — не изменяйте 
-		/// содержимое этого метода с помощью редактора кода.
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.textBox3 = new System.Windows.Forms.TextBox();
-			this.textBox4 = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.label6 = new System.Windows.Forms.Label();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// textBox1
+			// menuStrip1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(12, 25);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(31, 20);
-			this.textBox1.TabIndex = 0;
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(797, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
 			// 
-			// textBox2
+			// newToolStripMenuItem
 			// 
-			this.textBox2.Location = new System.Drawing.Point(49, 25);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(31, 20);
-			this.textBox2.TabIndex = 1;
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+			this.newToolStripMenuItem.Text = "New";
+			this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
 			// 
-			// textBox3
+			// exitToolStripMenuItem
 			// 
-			this.textBox3.Location = new System.Drawing.Point(105, 25);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(31, 20);
-			this.textBox3.TabIndex = 2;
-			// 
-			// textBox4
-			// 
-			this.textBox4.Location = new System.Drawing.Point(142, 25);
-			this.textBox4.Name = "textBox4";
-			this.textBox4.Size = new System.Drawing.Size(31, 20);
-			this.textBox4.TabIndex = 3;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(86, 28);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(13, 13);
-			this.label1.TabIndex = 4;
-			this.label1.Text = "+";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(179, 28);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(13, 13);
-			this.label2.TabIndex = 5;
-			this.label2.Text = "=";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(198, 28);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(50, 13);
-			this.label3.TabIndex = 6;
-			this.label3.Text = "RESULT";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(24, 9);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(46, 13);
-			this.label4.TabIndex = 7;
-			this.label4.Text = "complex";
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(117, 9);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(46, 13);
-			this.label5.TabIndex = 8;
-			this.label5.Text = "complex";
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(12, 51);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 9;
-			this.button1.Text = "add";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(93, 51);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 10;
-			this.button2.Text = "sub";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(174, 51);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(75, 23);
-			this.button3.TabIndex = 11;
-			this.button3.Text = "mul";
-			this.button3.UseVisualStyleBackColor = true;
-			this.button3.Click += new System.EventHandler(this.button3_Click);
-			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(102, 77);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(45, 13);
-			this.label6.TabIndex = 12;
-			this.label6.Text = "purpose";
-			this.label6.Visible = false;
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.exitToolStripMenuItem.Text = "Exit";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(259, 160);
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.textBox4);
-			this.Controls.Add(this.textBox3);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
+			this.ClientSize = new System.Drawing.Size(797, 281);
+			this.Controls.Add(this.menuStrip1);
+			this.IsMdiContainer = true;
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -183,19 +78,8 @@
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.TextBox textBox3;
-		private System.Windows.Forms.TextBox textBox4;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 	}
 }
-
--- calculator/Form1.cs
+++ calculator/Form1.cs
@@ -4,11 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace calculator
 {
@@ -17,168 +15,15 @@ namespace calculator
 		public Form1()
 		{
 			InitializeComponent();
-
-			if (!File.Exists(@"C:/Users/Ikon/Desktop/Work/OS&SP/lab1/addition/bin/Debug/addition.dll"))
-			{
-				button1.Enabled = false;
-			}
-			if (!File.Exists(@"C:/Users/Ikon/Desktop/Work/OS&SP/lab1/subtraction/bin/Debug/subtraction.dll"))
-			{
-				button2.Enabled = false;
-			}
-			if (!File.Exists(@"C:/Users/Ikon/Desktop/Work/OS&SP/lab1/multiplication/bin/Debug/multiplication.dll"))
-			{
-				button3.Enabled = false;
-			}
 		}
 
-		public string Complex(int a, int b)
+		private void newToolStripMenuItem_Click(object sender, EventArgs e)
 		{
-			if (a != 0)
-			{
-				if (b < 0)
-					return (a).ToString() + (b).ToString() + "i";
-				else if (b > 0)
-					return (a).ToString() + "+" + (b).ToString() + "i";
-				else
-					return (a).ToString();
-			}
-			else
-			{
-				if (b != 0)
-					return (b).ToString() + "i";
-				else
-					return "0";
-			}
-		}
-
-		private void button1_Click(object sender, EventArgs e)
-		{
-			button1.Enabled = true;
-			button2.Enabled = true;
-			button3.Enabled = true;
-			try
-			{
-				if (textBox1.Text == "" || textBox2.Text == "")
-					return;
-
-				Assembly asm = Assembly.LoadFrom(@"C:/Users/Ikon/Desktop/Work/OS&SP/lab1/addition/bin/Debug/addition.dll");
-				Type t = asm.GetType("addition", true, true);
-				object obj = Activator.CreateInstance(t);
-				MethodInfo add = t.GetMethod("add");
-				Object res;
-
-				MethodInfo purpose = t.GetMethod("purpose");
-				Object purp;
-
-				int a1 = int.Parse(textBox1.Text);
-				int b1 = int.Parse(textBox2.Text);
-				int a2 = int.Parse(textBox3.Text);
-				int b2 = int.Parse(textBox4.Text);
-
-				label4.Text = Complex(a1, b1);
-				label5.Text = Complex(a2, b2);
-				label1.Text = "+";
-
-				purp = purpose.Invoke(obj, new object[] { });
-				label6.Visible = true;
-				label6.Text = purp.ToString();
-
-				res = add.Invoke(obj, new object[] { a1, b1, a2, b2 });
-				label3.Text = res.ToString();
-			}
-			catch (Exception ex)
-			{
-				label6.Visible = false;
-				button1.Enabled = false;
-				MessageBox.Show("Could not load DLL with addition function or input error");
-			}
-		}
-
-		private void button2_Click(object sender, EventArgs e)
-		{
-			button1.Enabled = true;
-			button2.Enabled = true;
-			button3.Enabled = true;
-			try
-			{
-				if (textBox1.Text == "" || textBox2.Text == "")
-					return;
-
-				Assembly asm = Assembly.LoadFrom(@"C:/Users/Ikon/Desktop/Work/OS&SP/lab1/subtraction/bin/Debug/subtraction.dll");
-				Type t = asm.GetType("subtraction", true, true);
-				object obj = Activator.CreateInstance(t);
-				MethodInfo sub = t.GetMethod("sub");
-				Object res;
-
-				MethodInfo purpose = t.GetMethod("purpose");
-				Object purp;
-
-				int a1 = int.Parse(textBox1.Text);
-				int b1 = int.Parse(textBox2.Text);
-				int a2 = int.Parse(textBox3.Text);
-				int b2 = int.Parse(textBox4.Text);
-
-				label4.Text = Complex(a1, b1);
-				label5.Text = Complex(a2, b2);
-				label1.Text = "-";
-
-				purp = purpose.Invoke(obj, new object[] { });
-				label6.Visible = true;
-				label6.Text = purp.ToString();
-
-				res = sub.Invoke(obj, new object[] { a1, b1, a2, b2 });
-				label3.Text = res.ToString();
-			}
-			catch (Exception ex)
-			{
-				label6.Visible = false;
-				button2.Enabled = false;
-				MessageBox.Show("Could not load DLL with subtraction function or input error");
-			}
-		}
-
-		private void button3_Click(object sender, EventArgs e)
-		{
-			button1.Enabled = true;
-			button2.Enabled = true;
-			button3.Enabled = true;
-			try
-			{
-				if (textBox1.Text == "" || textBox2.Text == "")
-					return;
-
-				Assembly asm = Assembly.LoadFrom(@"C:/Users/Ikon/Desktop/Work/OS&SP/lab1/multiplication/bin/Debug/multiplication.dll");
-				Type t = asm.GetType("multiplication", true, true);
-				object obj = Activator.CreateInstance(t);
-				MethodInfo mul = t.GetMethod("mul");
-				Object res;
-
-				MethodInfo purpose = t.GetMethod("purpose");
-				Object purp;
-
-				int a1 = int.Parse(textBox1.Text);
-				int b1 = int.Parse(textBox2.Text);
-				int a2 = int.Parse(textBox3.Text);
-				int b2 = int.Parse(textBox4.Text);
-
-				label4.Text = Complex(a1, b1);
-				label5.Text = Complex(a2, b2);
-				label1.Text = "*";
-
-				purp = purpose.Invoke(obj, new object[] { });
-				label6.Visible = true;
-				label6.Text = purp.ToString();
-
-				res = mul.Invoke(obj, new object[] { a1, b1, a2, b2 });
-				label3.Text = res.ToString();
-			}
-			catch (Exception ex)
-			{
-				label6.Visible = false;
-				button3.Enabled = false;
-				MessageBox.Show("Could not load DLL with multiplication function or input error");
-			}
+			Form2 newMDIChild = new Form2();
+			// Set the Parent Form of the Child window.  
+			newMDIChild.MdiParent = this;
+			// Display the new form.  
+			newMDIChild.Show();
 		}
 	}
 }

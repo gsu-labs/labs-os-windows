54c97cb2be89964b32e3ecbe42d43143e4f2350d
EmelyanovSI <48243046+EmelyanovSI@users.noreply.github.com>
2019-03-09 16:31:19 +0300

    lab2

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -41,7 +41,7 @@
             this.exitToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(797, 24);
+			this.menuStrip1.Size = new System.Drawing.Size(534, 24);
 			this.menuStrip1.TabIndex = 1;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -62,7 +62,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(797, 281);
+			this.ClientSize = new System.Drawing.Size(534, 207);
 			this.Controls.Add(this.menuStrip1);
 			this.IsMdiContainer = true;
 			this.MainMenuStrip = this.menuStrip1;

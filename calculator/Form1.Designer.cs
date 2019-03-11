2330012e8ee9517ad30928f45c100b2c3998187a
EmelyanovSI <48243046+EmelyanovSI@users.noreply.github.com>
2019-03-11 19:28:26 +0300

    icon added

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.textBox3 = new System.Windows.Forms.TextBox();
@@ -174,8 +175,9 @@
 			this.Controls.Add(this.textBox3);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Calculator";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 

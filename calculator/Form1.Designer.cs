1d474f78b661c2f9450e6b907d31945a732df4ae
EmelyanovSI <48243046+EmelyanovSI@users.noreply.github.com>
2019-03-26 16:07:27 +0300

    lab1 fixed

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.textBox3 = new System.Windows.Forms.TextBox();
@@ -174,6 +175,7 @@
 			this.Controls.Add(this.textBox3);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
 			this.Text = "Calculator";
 			this.ResumeLayout(false);

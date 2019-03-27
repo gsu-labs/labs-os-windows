f9dd365743be79ac379acea36aa9d7a7075813ba
EmelyanovSI <48243046+EmelyanovSI@users.noreply.github.com>
2019-03-27 12:10:50 +0300

    lab4 complete

--- calculator/Form1.cs
+++ calculator/Form1.cs
@@ -26,8 +26,7 @@ namespace calculator
 
 		string PathInput = Directory.GetCurrentDirectory();
 		string PathOutput = Directory.GetCurrentDirectory();
-
-		string ClacPath = Directory.GetCurrentDirectory();
+		string CalcPath = Directory.GetCurrentDirectory();
 
 		public Form1()
 		{
@@ -37,60 +36,39 @@ namespace calculator
 			{
 				PathInput = PathInput.Substring(0, PathInput.LastIndexOf("\\"));
 				PathOutput = PathOutput.Substring(0, PathOutput.LastIndexOf("\\"));
-				ClacPath = ClacPath.Substring(0, ClacPath.LastIndexOf("\\"));
+				CalcPath = CalcPath.Substring(0, CalcPath.LastIndexOf("\\"));
 			}
 
 			PathInput += "\\input.txt";
 			PathOutput += "\\output.txt";
-			ClacPath += "\\calculatorX\\bin\\Debug\\calculatorX.exe";
+			CalcPath += "\\calculatorX\\bin\\Debug\\calculatorX.exe";
 		}
 
-		public string Complex(int a, int b)
-		{
-			if (a != 0)
-			{
-				if (b < 0)
-					return (a).ToString() + (b).ToString() + "i";
-				else if (b > 0)
-					return (a).ToString() + "+" + (b).ToString() + "i";
-				else
-					return (a).ToString();
-			}
-			else
-			{
-				if (b != 0)
-					return (b).ToString() + "i";
-				else
-					return "0";
-			}
-		}
-
-		public void GetComplex()
+		public void ComplexCalculator()
 		{
 			if (textBox1.Text == "" || textBox2.Text == "" ||
 				textBox3.Text == "" || textBox4.Text == "")
 				return;
 
-			string request = "";
-			request = textBox1.Text + " " + textBox2.Text + " " + textBox3.Text + " " + textBox4.Text;
-			request += " " + label1.Text;
+			string str = textBox1.Text + " " + textBox2.Text + " " + textBox3.Text + " " + textBox4.Text;
+			str += " " + label1.Text;
 
 			using (StreamWriter sw = new StreamWriter(PathInput, false, Encoding.Default))
 			{
-				sw.Write(request);
+				sw.Write(str);
 			}
 
 			uint arg = 2;
 
 			if (radioButton1.Checked)
-				ShellExecuteA(this.Handle, "open", @"D:\ОС\labs\Lab4_os\Console\Console\bin\Debug\Console.exe", null, null, 1);
+				ShellExecuteA(this.Handle, "open", CalcPath, null, null, 1);
 			else
-				ShellExecuteA(this.Handle, "open", @"D:\ОС\labs\Lab4_os\Console\Console\bin\Debug\Console.exe", null, null, 0);
+				ShellExecuteA(this.Handle, "open", CalcPath, null, null, 0);
 
 			int process = 0;
 			Process[] pp = Process.GetProcesses();
 			for (int i = 0; i < pp.Length; i++)
-				if (pp[i].ProcessName == "Console")
+				if (pp[i].ProcessName == "calculatorX")
 				{
 					process = i;
 					break;
@@ -102,146 +80,37 @@ namespace calculator
 			}
 
 			StreamReader sr = new StreamReader(PathOutput, Encoding.Default);
-			string answer = sr.ReadToEnd();
+			string result = sr.ReadToEnd();
 			sr.Close();
 			sr.Dispose();
 
-			label3.Text = answer;
+			String[] words = result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+			label3.Text = words[2];
+			label4.Text = words[0];
+			label5.Text = words[1];
+			label6.Visible = true;
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
 			label1.Text = "+";
-			GetComplex();
-
-			//button1.Enabled = true;
-			//button2.Enabled = true;
-			//button3.Enabled = true;
-			//try
-			//{
-			//	if (textBox1.Text == "" || textBox2.Text == "" ||
-			//		textBox3.Text == "" || textBox4.Text == "")
-			//		return;
-
-			//	Assembly asm = Assembly.LoadFrom(additionPath);
-			//	Type t = asm.GetType("addition", true, true);
-			//	object obj = Activator.CreateInstance(t);
-			//	MethodInfo add = t.GetMethod("add");
-			//	Object res;
-
-			//	MethodInfo purpose = t.GetMethod("purpose");
-			//	Object purp;
-
-			//	int a1 = int.Parse(textBox1.Text);
-			//	int b1 = int.Parse(textBox2.Text);
-			//	int a2 = int.Parse(textBox3.Text);
-			//	int b2 = int.Parse(textBox4.Text);
-
-			//	label4.Text = Complex(a1, b1);
-			//	label5.Text = Complex(a2, b2);
-			//	label1.Text = "+";
-
-			//	purp = purpose.Invoke(obj, new object[] { });
-			//	label6.Visible = true;
-			//	label6.Text = purp.ToString();
-
-			//	res = add.Invoke(obj, new object[] { a1, b1, a2, b2 });
-			//	label3.Text = res.ToString();
-			//}
-			//catch (Exception ex)
-			//{
-			//	label6.Visible = false;
-			//	button1.Enabled = false;
-			//	MessageBox.Show("Could not load DLL with addition function or input error");
-			//}
+			label6.Text = button1.Text;
+			ComplexCalculator();
 		}
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-			button1.Enabled = true;
-			button2.Enabled = true;
-			button3.Enabled = true;
-			try
-			{
-				if (textBox1.Text == "" || textBox2.Text == "" ||
-					textBox3.Text == "" || textBox4.Text == "")
-					return;
-
-				Assembly asm = Assembly.LoadFrom(subtractionPath);
-				Type t = asm.GetType("subtraction", true, true);
-				object obj = Activator.CreateInstance(t);
-				MethodInfo sub = t.GetMethod("sub");
-				Object res;
-
-				MethodInfo purpose = t.GetMethod("purpose");
-				Object purp;
-
-				int a1 = int.Parse(textBox1.Text);
-				int b1 = int.Parse(textBox2.Text);
-				int a2 = int.Parse(textBox3.Text);
-				int b2 = int.Parse(textBox4.Text);
-
-				label4.Text = Complex(a1, b1);
-				label5.Text = Complex(a2, b2);
-				label1.Text = "-";
-
-				purp = purpose.Invoke(obj, new object[] { });
-				label6.Visible = true;
-				label6.Text = purp.ToString();
-
-				res = sub.Invoke(obj, new object[] { a1, b1, a2, b2 });
-				label3.Text = res.ToString();
-			}
-			catch (Exception ex)
-			{
-				label6.Visible = false;
-				button2.Enabled = false;
-				MessageBox.Show("Could not load DLL with subtraction function or input error");
-			}
+			label1.Text = "-";
+			label6.Text = button2.Text;
+			ComplexCalculator();
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
-			button1.Enabled = true;
-			button2.Enabled = true;
-			button3.Enabled = true;
-			try
-			{
-				if (textBox1.Text == "" || textBox2.Text == "" ||
-					textBox3.Text == "" || textBox4.Text == "")
-					return;
-
-				Assembly asm = Assembly.LoadFrom(multiplicationPath);
-				Type t = asm.GetType("multiplication", true, true);
-				object obj = Activator.CreateInstance(t);
-				MethodInfo mul = t.GetMethod("mul");
-				Object res;
-
-				MethodInfo purpose = t.GetMethod("purpose");
-				Object purp;
-
-				int a1 = int.Parse(textBox1.Text);
-				int b1 = int.Parse(textBox2.Text);
-				int a2 = int.Parse(textBox3.Text);
-				int b2 = int.Parse(textBox4.Text);
-
-				label4.Text = Complex(a1, b1);
-				label5.Text = Complex(a2, b2);
-				label1.Text = "*";
-
-				purp = purpose.Invoke(obj, new object[] { });
-				label6.Visible = true;
-				label6.Text = purp.ToString();
-
-				res = mul.Invoke(obj, new object[] { a1, b1, a2, b2 });
-				label3.Text = res.ToString();
-			}
-			catch (Exception ex)
-			{
-				label6.Visible = false;
-				button3.Enabled = false;
-				MessageBox.Show("Could not load DLL with multiplication function or input error");
-			}
+			label1.Text = "*";
+			label6.Text = button3.Text;
+			ComplexCalculator();
 		}
 	}
 }
--- calculatorX/Program.cs
+++ calculatorX/Program.cs
@@ -9,9 +9,30 @@ namespace calculatorX
 {
 	class Program
 	{
+		public static string Complex(int a, int b)
+		{
+			if (a != 0)
+			{
+				if (b < 0)
+					return (a).ToString() + (b).ToString() + "i";
+				else if (b > 0)
+					return (a).ToString() + "+" + (b).ToString() + "i";
+				else
+					return (a).ToString();
+			}
+			else
+			{
+				if (b != 0)
+					return (b).ToString() + "i";
+				else
+					return "0";
+			}
+		}
+
 		static void Main(string[] args)
 		{
 			Console.SetWindowSize(40, 10);
+
 			string PathInput = Directory.GetCurrentDirectory();
 			string PathOutput = Directory.GetCurrentDirectory();
 
@@ -20,40 +41,37 @@ namespace calculatorX
 				PathInput = PathInput.Substring(0, PathInput.LastIndexOf("\\"));
 				PathOutput = PathOutput.Substring(0, PathOutput.LastIndexOf("\\"));
 			}
-
-			PathInput = "\\input.txt";
-			PathOutput = "\\output.txt";
+			PathInput += "\\input.txt";
+			PathOutput += "\\output.txt";
 
 			StreamReader sr = new StreamReader(PathInput, Encoding.Default);
-			string input = sr.ReadToEnd();
+			string str = sr.ReadToEnd();
 			sr.Close();
 			sr.Dispose();
 
-			String[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			String[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
 			using (StreamWriter sw = new StreamWriter(PathOutput, false, Encoding.Default))
 			{
+				str = str.Remove(0, 9);
+				str += Complex(int.Parse(words[0]), int.Parse(words[1])) + " ";
+				str += Complex(int.Parse(words[2]), int.Parse(words[3])) + " ";
 				if (words[4] == "+")
 				{
-					input = input.Remove(0, 1);
-					addition additionX = new addition();
-					sw.Write(additionX.add(int.Parse(words[0]), int.Parse(words[1]), 
-											int.Parse(words[2]), int.Parse(words[3])));
+					addition addx = new addition();
+					str += addx.add(int.Parse(words[0]), int.Parse(words[1]), int.Parse(words[2]), int.Parse(words[3]));
 				}
 				else if (words[4] == "-")
 				{
-					input = input.Remove(0, 1);
-					subtraction subtractionX = new subtraction();
-					sw.Write(subtractionX.sub(int.Parse(words[0]), int.Parse(words[1]),
-											int.Parse(words[2]), int.Parse(words[3])));
+					subtraction subx = new subtraction();
+					str += subx.sub(int.Parse(words[0]), int.Parse(words[1]), int.Parse(words[2]), int.Parse(words[3]));
 				}
 				else
 				{
-					input = input.Remove(0, 1);
-					multiplication multiplicationX = new multiplication();
-					sw.Write(multiplicationX.mul(int.Parse(words[0]), int.Parse(words[1]),
-											int.Parse(words[2]), int.Parse(words[3])));
+					multiplication mulx = new multiplication();
+					str += mulx.mul(int.Parse(words[0]), int.Parse(words[1]), int.Parse(words[2]), int.Parse(words[3]));
 				}
+				sw.Write(str);
 			}
 		}
 	}
